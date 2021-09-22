9fa6facc562723b9a62bbf8c8a8a5f0a7d40efbf
Ang Way Shen <wayshen.a@vettons.com>
2021-09-22 16:26:40 +0800

    Product & Stock API fixed

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import path from 'path'
-import * as routes from './routes'
+import {register} from './src/routes'
 
 dotenv.config()
 const port = process.env.SERVER_PORT
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+register(app)
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,27 @@
-import * as express from 'express'
+import {Request, Response, NextFunction, Application} from 'express'
+
 import fs from 'fs'
 
-export const register = (app: express.Application) => {
-  app.post('/order/complete', (req: any, res) => {})
-  app.post('/stock/update', (req: any, res) => {})
-  app.get('/stock/decrease', (req: any, res) => {})
+export const register = (app: Application) => {
+  app.post('/order/complete', (req: Request, res: Response) => {
+    const date = Date.now()
+    const fileName = `./src/log/order-complete/${date}.txt`
+    const data = `${date} ${JSON.stringify(req.body)} \n\r`
+    fs.writeFileSync(fileName, data, 'utf-8')
+    return res.json({status: 'order complete log success'})
+  })
+  app.post('/stock/update', (req: Request, res: Response) => {
+    const date = Date.now()
+    const fileName = `./src/log/stock-update/${date}.txt`
+    const data = `${date} ${JSON.stringify(req.body)} \n\r`
+    fs.writeFileSync(fileName, data, 'utf-8')
+    return res.json({status: 'stock update log success'})
+  })
+  app.get('/stock/decrease', (req: Request, res: Response) => {
+    const date = Date.now()
+    const fileName = `./src/log/stock-decrease/${date}.txt`
+    const data = `${date} ${JSON.stringify(req.body)} \n\r`
+    fs.writeFileSync(fileName, data, 'utf-8')
+    return res.json({status: 'stock decrease log success'})
+  })
 }
